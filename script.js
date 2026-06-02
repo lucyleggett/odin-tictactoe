b7f252f740b24b2fef464cdba747e13e50954d2e
lucyleggett <lucy.leggett@hotmail.com>
2026-06-02 13:02:17 +0800

    Create gameBoard and game Controller functions

---
+++ script.js
@@ -0,0 +1,35 @@
+/* Pseudocode
+
+1. Create gameBoard array, using a for loop to create 9 objects representing each square of the board.
+2. Create gameController function, which will contain player1 and player2 objects. Will track if they're "X" or "O", number of turns taken and which square of the board they last played on.
+3. 
+
+*/
+
+function gameBoard() {
+    const cells = 9;
+    const board = [];
+
+    for (let i = 0; i < cells; i++) {
+        board.push(i);
+    }
+}
+
+function gameController() {
+    const players = [
+        {
+            name: "Player One",
+            token: 1,
+        },
+        {
+            name: "Player Two",
+            token: 2,
+        }
+    ]
+    let activePlayer = players[0];
+
+    const getActivePlayer = () => {activePlayer};
+    const switchPlayerTurn = () => {
+        activePlayer = activePlayer === players[0] ? players[1] : players [0];
+    };
+}
